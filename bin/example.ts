68251c0cd5bc9549337ba5a0370c08fe3a379211
Myles Loffler <myles2007@gmail.com>
2023-03-30 18:46:10 -0500

    clean: remove unused import

--- bin/example.ts
+++ bin/example.ts
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
 import { TrustStack } from "../lib/TrustStack";
 import { LambdaStack } from "../lib/LambdaStack";
